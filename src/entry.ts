12604ea437d52129f4a283d3ca9d03064462c312
1391020381 <1391020381@qq.com>
2022-11-08 15:40:52 +0800

    husky

--- src/entry.ts
+++ src/entry.ts
@@ -2,9 +2,7 @@ import { App } from "vue";
 import MyButton from "./button";
 import SFCButton from "./SFCButton.vue";
 import JSXButton from "./JSXButton";
-
 export { MyButton, SFCButton, JSXButton };
-
 export default {
   install(app: App): void {
     app.component(MyButton.name, MyButton);
